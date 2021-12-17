7905aa203b71d3f57d2c7318a35c888210a9a18d
Poojitha <poojitha.bolla@motivitylabs.com>
2021-12-17 12:27:40 +0530

    Application Testcases

---
+++ app/src/main/java/com/example/application/SharedPreferencesHelper.java
@@ -0,0 +1,42 @@
+package com.example.application;
+
+import android.content.SharedPreferences;
+
+import java.util.Calendar;
+
+public class SharedPreferencesHelper {
+
+    static final String KEY_NAME = "key_name";
+    static final String KEY_DOB = "key_dob_millis";
+    static final String KEY_EMAIL = "key_email";
+
+    private SharedPreferences mSharedPreferences;
+
+
+
+    public  SharedPreferencesHelper(SharedPreferences sharedPreferences) {
+        mSharedPreferences = sharedPreferences;
+    }
+
+    public boolean savePersonalInfo(SharedPreferenceEntry sharedPreferenceEntry){
+
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(KEY_NAME, sharedPreferenceEntry.getName());
+        editor.putLong(KEY_DOB, sharedPreferenceEntry.getDateOfBirth().getTimeInMillis());
+        editor.putString(KEY_EMAIL, sharedPreferenceEntry.getEmail());
+
+        return editor.commit();
+    }
+
+    public SharedPreferenceEntry getPersonalInfo() {
+
+        String name = mSharedPreferences.getString(KEY_NAME, "");
+        Long dobMillis =
+                mSharedPreferences.getLong(KEY_DOB, Calendar.getInstance().getTimeInMillis());
+        Calendar dateOfBirth = Calendar.getInstance();
+        dateOfBirth.setTimeInMillis(dobMillis);
+        String email = mSharedPreferences.getString(KEY_EMAIL, "");
+
+        return new SharedPreferenceEntry(name, dateOfBirth, email);
+    }
+}
